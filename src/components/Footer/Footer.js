8ffb76a0691926050f7861d3f90d8b0862ad4fcc
rasel <russelroy15@gmail.com>
2021-09-20 21:44:08 +0600

    WIP on rest countries React

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        <small>&copy; Copyright protected by Rasel</small>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
+
+//rsc
