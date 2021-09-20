4033288da4929cc7047090a91f50d42dc35cffc8
rasel <russelroy15@gmail.com>
2021-09-20 20:16:08 +0600

    WIP on rest countries React

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Country.css";
 
 const Country = (props) => {
   //   console.log(props);
   return (
-    <div>
+    <div className="Country">
       <h4>Name: {props.name}</h4>
       <h5>
         Capital is: {props.capital} and Population is: {props.population}
