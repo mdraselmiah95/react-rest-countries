d32e22f75bb7505dad4f31870228087722921661
rasel <russelroy15@gmail.com>
2021-09-20 20:35:55 +0600

    WIP on rest countries React

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./Country.css";
 
 const Country = (props) => {
-  const { name, flag, capital, population } = props.country;
+  const { name, flag, capital, population, region } = props.country;
   //   console.log(props.country);
   return (
     <div className="Country">
       <h4>Name: {name}</h4>
       <img src={flag} alt="" />
-      <h5>
+      <p>
+        <small>
+          <small>Region: {region}</small>
+        </small>
+      </p>
+      <p>
         Capital is: {capital} and Population is: {population}
-      </h5>
+      </p>
     </div>
   );
 };
