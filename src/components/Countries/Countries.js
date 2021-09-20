f2ca93df51b7628c887182021f780800ee6cab8e
rasel <russelroy15@gmail.com>
2021-09-20 20:46:42 +0600

    WIP on rest countries React

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -11,9 +12,11 @@ const Countries = () => {
   return (
     <div>
       <h1>This is Countries its own file. {countries.length}</h1>
-      {countries.map((country) => (
-        <Country country={country}></Country>
-      ))}
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country country={country}></Country>
+        ))}
+      </div>
     </div>
   );
 };
