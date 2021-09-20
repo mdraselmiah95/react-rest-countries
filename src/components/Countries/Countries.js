47aa3ef0b37180a0564ab29146e53317ea2db3cd
rasel <russelroy15@gmail.com>
2021-09-20 19:42:33 +0600

    WIP on rest countries React

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
   return (
     <div>
-      <h1>This is Countries its own file.</h1>
+      <h1>This is Countries its own file. {countries.length}</h1>
     </div>
   );
 };
